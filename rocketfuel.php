0b6c8fd631f4a3d3b58ee79ce5a29567ccf6ce40
Михаил В <mvvershinin@gmail.com>
2021-01-18 15:08:48 +0700

    Admin settings
    make admin settings for plpugin

--- rocketfuel.php
+++ rocketfuel.php
@@ -23,17 +23,17 @@ class RocketFuel extends PaymentModule
      */
     public function __construct()
     {
-        $this->name                   = 'rocketfuel';
-        $this->tab                    = 'payments_gateways';
-        $this->version                = '0.1';
-        $this->author                 = 'RocketFuel Inc.';
-        $this->controllers            = array('payment', 'validation');
-        $this->currencies             = true;
-        $this->currencies_mode        = 'checkbox';
-        $this->bootstrap              = true;
-        $this->displayName            = 'RocketFuel';
-        $this->description            = 'The RocketFuel blockchain based one-click “BUY-NOW” check-out solution is a game-changing technology that promotes remarkable high conversion efficiencies and further stimulates highly impulsive buying in e-commerce scenarios.';
-        $this->confirmUninstall       = 'Are you sure you want to uninstall this module?';
+        $this->name = 'rocketfuel';
+        $this->tab = 'payments_gateways';
+        $this->version = '0.1';
+        $this->author = 'RocketFuel Inc.';
+        $this->controllers = array('payment', 'validation');
+        $this->currencies = true;
+        $this->currencies_mode = 'checkbox';
+        $this->bootstrap = true;
+        $this->displayName = 'RocketFuel';
+        $this->description = 'The RocketFuel blockchain based one-click “BUY-NOW” check-out solution is a game-changing technology that promotes remarkable high conversion efficiencies and further stimulates highly impulsive buying in e-commerce scenarios.';
+        $this->confirmUninstall = 'Are you sure you want to uninstall this module?';
         $this->ps_versions_compliancy = array('min' => '1.7.0', 'max' => _PS_VERSION_);
 
         parent::__construct();
@@ -65,11 +65,101 @@ class RocketFuel extends PaymentModule
      * Returns a string containing the HTML necessary to
      * generate a configuration screen on the admin
      *
+     * form {
+     *  merchant_id
+     *  address_for_rocketfuel_callback
+     * }
      * @return string
      */
     public function getContent()
     {
-        return $this->_html;
+        $output = null;
+
+        if (Tools::isSubmit('submit' . $this->name)) {
+            $myModuleName = strval(Tools::getValue('ROCKETFUEL_MERCHANT_ID'));
+
+            if (
+                !$myModuleName ||
+                empty($myModuleName) ||
+                !Validate::isGenericName($myModuleName)
+            ) {
+                $output .= $this->displayError($this->l('Invalid Configuration value'));
+            } else {
+                Configuration::updateValue('ROCKETFUEL_MERCHANT_ID', $myModuleName);
+                $output .= $this->displayConfirmation($this->l('Settings updated'));
+            }
+        }
+
+        return $output . $this->displayForm();
+    }
+
+    /**
+     * Display form for configure on the admin
+     *
+     * @return mixed
+     */
+    public function displayForm()
+    {
+        // Get default language
+        $defaultLang = (int)Configuration::get('PS_LANG_DEFAULT');
+
+        // Init Fields form array
+        $fieldsForm[0]['form'] = [
+            'legend' => [
+                'title' => $this->l('Settings'),
+                'test' => 'test text'
+            ],
+            'description' => 'Callback URL for RocketFuel is <b>' . $this->getCallbackUrl() .'</b>',
+            'input' => [
+                [
+                    'type' => 'text',
+                    'label' => $this->l('RocketFuel Merchant ID'),
+                    'name' => 'ROCKETFUEL_MERCHANT_ID',
+                    'size' => 20,
+                    'required' => true
+                ],
+
+            ],
+            'div' => 'test',
+            'submit' => [
+                'title' => $this->l('Save'),
+                'class' => 'btn btn-default pull-right'
+            ]
+        ];
+
+        $helper = new HelperForm();
+
+        // Module, token and currentIndex
+        $helper->module = $this;
+        $helper->name_controller = $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex . '&configure=' . $this->name;
+
+        // Language
+        $helper->default_form_language = $defaultLang;
+        $helper->allow_employee_form_lang = $defaultLang;
+
+        // Title and toolbar
+        $helper->title = $this->displayName;
+        $helper->show_toolbar = true;        // false -> remove toolbar
+        $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
+        $helper->submit_action = 'submit' . $this->name;
+        $helper->toolbar_btn = [
+            'save' => [
+                'desc' => $this->l('Save'),
+                'href' => AdminController::$currentIndex . '&configure=' . $this->name . '&save' . $this->name .
+                    '&token=' . Tools::getAdminTokenLite('AdminModules'),
+            ],
+            'back' => [
+                'href' => AdminController::$currentIndex . '&token=' . Tools::getAdminTokenLite('AdminModules'),
+                'desc' => $this->l('Back to list')
+            ]
+        ];
+
+        // Load current value
+        $helper->fields_value['ROCKETFUEL_MERCHANT_ID'] = Tools::getValue('ROCKETFUEL_MERCHANT_ID', Configuration::get('ROCKETFUEL_MERCHANT_ID'));
+
+        return $helper->generateForm($fieldsForm);
     }
 
     /**
@@ -123,7 +213,7 @@ class RocketFuel extends PaymentModule
 
     /**
      * Display a message in the paymentReturn hook
-     * 
+     *
      * @param array $params
      * @return string
      */
@@ -137,5 +227,15 @@ class RocketFuel extends PaymentModule
         }
 
         return $this->fetch('module:rocketfuel/views/templates/hook/payment_return.tpl');
+    }
+
+    /**
+     * Get url for rocketfuel callback
+     *
+     * @return string
+     */
+    protected function getCallbackUrl()
+    {
+        return Configuration::get('PS_SHOP_DOMAIN') . '/rocketfuel-callback';
     }
 }
