349803663117d086b65090b6aeda542e3b7c7819
Udor Blessingm <berachah54@gmail.com>
2022-02-03 13:59:19 +0100

    First commit

---
+++ rocketfuel.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * Rocketfuel Payment Gateway for Prestashop - A Sample Payment Module for PrestaShop 1.7
+ *
+ * 
+ *
+ * @author Udor Blessing
+ * 
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Rocketfuel extends PaymentModule
+{
+
+    private $_html = '';
+    private $_postErrors = array();
+
+    public $address;
+    /**
+     * PrestaPay constructor.
+     *
+     * Set the information about this module
+     */
+    public function __construct()
+    {
+        $this->name                   = 'rocketfuel';
+        $this->tab                    = 'payments_gateways';
+        $this->version                = '1.0';
+        $this->author                 = 'Rocketfuel Team';
+        $this->controllers            = array('payment', 'validation');
+        $this->currencies             = true;
+        $this->currencies_mode        = 'checkbox';
+        $this->bootstrap              = true;
+        $this->displayName            = 'Rocketfuel';
+        $this->description            = 'Sample Payment module developed for learning purposes.';
+        $this->confirmUninstall       = 'Are you sure you want to uninstall this module?';
+        $this->ps_versions_compliancy = array('min' => '1.7.0', 'max' => _PS_VERSION_);
+
+        parent::__construct();
+    }
+    /**
+     * Install this module and register the following Hooks:
+     *
+     * @return bool
+     */
+    public function install()
+    {
+        return parent::install()
+        && $this->registerHook('paymentOptions')
+        && $this->registerHook('paymentReturn');
+    }
+      /**
+     * Uninstall this module and remove it from all hooks
+     *
+     * @return bool
+     */
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+      /**
+     * Returns a string containing the HTML necessary to
+     * generate a configuration screen on the admin
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->_html;
+    }
+    
+    /**
+     * Display this module as a payment option during the checkout
+     *
+     * @param array $params
+     * @return array|void
+     */
+    public function hookPaymentOptions($params)
+    {
+        /*
+         * Verify if this module is active
+         */
+        if (!$this->active) {
+            return;
+        }
+
+        /**
+         * Form action URL. The form data will be sent to the
+         * validation controller when the user finishes
+         * the order process.
+         */
+        $formAction = $this->context->link->getModuleLink($this->name, 'validation', array(), true);
+
+        /**
+         * Assign the url form action to the template var $action
+         */
+        $this->smarty->assign(['action' => $formAction]);
+
+        /**
+         *  Load form template to be displayed in the checkout step
+         */
+        $paymentForm = $this->fetch('module:rocketfuel/views/templates/hook/payment_options.tpl');
+
+        /**
+         * Create a PaymentOption object containing the necessary data
+         * to display this module in the checkout
+         */
+        $newOption = new PrestaShop\PrestaShop\Core\Payment\PaymentOption;
+        
+        $newOption->setModuleName($this->displayName)
+            ->setCallToActionText($this->displayName)
+            ->setAction($formAction)
+            ->setForm($paymentForm);
+
+        $payment_options = array(
+            $newOption
+        );
+
+        return $payment_options;
+    }
+}
---
+++ views/templates/hook/payment_options.tpl
@@ -0,0 +1,10 @@
+{*
+ * RocketFuel - A Payment Module for PrestaShop 1.7
+ *
+ * Form to be displayed in the payment step
+ *
+ *}
+
+<form method="post" action="{$action}">
+    <P>Now you can check your order and go to payment page.</P>
+</form>
